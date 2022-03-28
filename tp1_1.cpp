486bab7119f5e5a88a7cdb15d46228b76b57aaf8
NicolasArias <ariasmec06@gmail.com>
2022-03-28 17:43:37 -0300

    Se sube tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
-    printf ("Hola mundo");
+    int variable1 = 20;
+    int * puntero_variable;
+    puntero_variable= &variable1;
+
+    printf("contenido del puntero: %p \n", puntero_variable);
+    printf("Direccion de memoria de la variable: %p\n", &variable1);
+    printf("Direccion de memoria del puntero: %p\n", &puntero_variable);
+    printf("Tamanio de memoria utilizado por la variable: %d\n", sizeof(variable1));
+    printf("El contenido de la variable: %d\n", *puntero_variable);
 
     getchar();
+    return 0;
 }
